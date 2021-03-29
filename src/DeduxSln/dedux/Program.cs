0f0d8753dd508502b15396a6fa921d45a30101d1
Кальянов Дмитрий <kalianovda@ekassir.com>
2021-03-29 16:22:41 +0300

    initial commit

---
+++ src/DeduxSln/dedux/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dedux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
